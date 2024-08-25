8fb37850dea49050b2ec0e71db6f045563c6173b
deevvan <116576756+deevvan@users.noreply.github.com>
2024-08-24 19:49:15 -0500

    Python script to create read counts after RSEM

---
+++ read_count_creator.py
@@ -0,0 +1,34 @@
+# Using genes.results files created by rsem-calculate-expression --star function, read_count (w/o normalization for DESeq2) matrix is created here:
+
+import os
+import pandas as pd
+
+# Define the input directory path
+input_dir = "/mmfs1/projects/changhui.yan/DeewanB/Manuscript3/RSEM/expression_host"
+
+# Initialize an empty dictionary to store data for each SRR ID
+data = {}
+
+# Iterate over subdirectories in the input directory
+for subdir in os.listdir(input_dir):
+    subdir_path = os.path.join(input_dir, subdir)
+    if os.path.isdir(subdir_path):
+        # Get the file path for the genes.results file
+        genes_results_file = os.path.join(subdir_path, f"{subdir}.genes.results")
+        if os.path.isfile(genes_results_file):
+            # Read the genes.results file into a DataFrame
+            df = pd.read_csv(genes_results_file, sep='\t')
+            # Extract gene_id and expected_count columns
+            df = df[['gene_id', 'expected_count']]
+            # Rename the columns to include the SRR ID
+            df.columns = ['gene_id', subdir]
+            # Set the gene_id column as the index
+            df.set_index('gene_id', inplace=True)
+            # Store the DataFrame in the dictionary
+            data[subdir] = df
+
+# Concatenate all DataFrames into one DataFrame
+count_table = pd.concat(data.values(), axis=1, sort=False)
+
+# Write the count table to a CSV file
+count_table.to_csv("/mmfs1/projects/changhui.yan/DeewanB/Manuscript3/RSEM/count_matrix_host.csv")
